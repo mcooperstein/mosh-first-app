c8ece1227485acfb5a2f24907f2a902bcca35b50
Marc Cooperstein <mcooperstein@gmail.com>
2018-02-17 12:23:43 -0800

    dependency injection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { CoursesComponent } from './courses.component';
 
 import { AppComponent } from './app.component';
-
+import { CoursesService } from './courses.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoursesComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    CoursesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
