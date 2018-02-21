f51c5e21fbd15f449fbda5cf8d4bb09235335867
Marc Cooperstein <mcooperstein@gmail.com>
2018-02-20 19:10:35 -0800

    added instructor solution using an array for prepositions

--- src/app/titlecase.pipe.ts
+++ src/app/titlecase.pipe.ts
@@ -9,9 +9,11 @@ export class TitlePipe implements PipeTransform {
     if(!value) {
       return null;
     } else {
+      let prepositions = ['of', 'the', 'and', 'by', 'or']
       let newStr = value.split(" ");
       for(let i =0; i<newStr.length; i++){
-        if(newStr[i]==="the"|| newStr[i]==="of" || newStr[i]==="and" || newStr[i]==="by"){
+        //if(newStr[i]==="the"|| newStr[i]==="of" || newStr[i]==="and" || newStr[i]==="by"){
+        if(prepositions.includes(newStr[i])) {
           if(i===0){
             newStr[i]= newStr[i][0].toUpperCase()+newStr[i].slice(1);
           }
