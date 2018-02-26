d7b76653aaeef0f9b219b8197f55e65fcf009d4a
Marc Cooperstein <mcooperstein@gmail.com>
2018-02-26 15:05:38 -0800

    http delete request

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -9,6 +9,7 @@ import { Http } from '@angular/http';
 export class PostsComponent implements OnInit {
   posts: any[];
   private url='https://jsonplaceholder.typicode.com/posts';
+  // Get
   constructor(private http: Http) {
     http.get(this.url)
       .subscribe(response =>{
@@ -18,7 +19,7 @@ export class PostsComponent implements OnInit {
 
   ngOnInit() {
   }
-
+  // Create
   createPost(input: HTMLInputElement){
     let userPost = {title:input.value}
     input.value = "";
@@ -27,6 +28,23 @@ export class PostsComponent implements OnInit {
         userPost['id'] = response.json().id;
         this.posts.splice(0,0,userPost);
         //console.log(response.json())
+      })
+  }
+  // Update
+  updatePost(post){
+    this.http.patch(this.url + '/' + post.id, JSON.stringify({isRead:true}))
+      .subscribe(response=>{
+        console.log(response.json());
+      })
+    //this.http.put(this.url, JSON.stringify(post))
+  }
+  // Delete
+
+  deletePost(post){
+    this.http.delete(this.url + '/' + post.id)
+      .subscribe(response=>{
+        let index = this.posts.indexOf(post);
+        this.posts.splice(index,1);
       })
   }
 
